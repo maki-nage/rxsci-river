6519d7e62039681561ecf844fc20ba94a9346818
Romain Picard <romain.picard@oakbits.com>
2021-05-22 21:58:52 +0200

    port prequential

--- rxsci_river/evaluate/prequential.py
+++ rxsci_river/evaluate/prequential.py
@@ -1,6 +1,5 @@
 import rx
-
-from rxsci_river.base import Utterance, Prediction
+from river import base
 
 
 def prequential(model, pretrain_size=200):
@@ -10,7 +9,7 @@ def prequential(model, pretrain_size=200):
     present, then the item is used for training. Otherwise only inference is done.
     
     Source: 
-        An observable emitting Utterance items.
+        An observable emitting tuples of (x, y) items.
 
     Args:
         model: A river model object
@@ -18,24 +17,48 @@ def prequential(model, pretrain_size=200):
         model before doing predictions.
 
     Returns:
-        An Observable emitting Prediction items for each input item. The
+        An Observable emitting prediction items for each input item. The
         firsts pretrain_size items do not emit predictions.
     """
+    if isinstance(model, base.Classifier):
+       predict = model.predict_one
+    elif isinstance(model, base.AnomalyDetector):
+        predict = model.score_one 
+    else:
+        raise NotImplementedError("prequential not implemented for model {}, contributions are welcome!".format(type(model)))
+
+    def _learn_one(i):
+        if type(i) is tuple:
+            model.learn_one(i[0], i[1])
+        elif type(i) is dict:
+            if 'y' in i:
+                model.learn_one({'x': i['x'], 'y': i['y']})
+            else:
+                model.learn_one({'x': i['x']})
+        else:
+            model.learn_one({'x': i})
+
+    def _predict(i):
+        if type(i) is tuple:
+            return predict(i[0])
+        elif type(i) is dict:
+            return predict({'x': i['x']})
+        else:
+            return predict({'x': i})
+
     def _prequential(source):
         def on_subscribe(observer, scheduler):
             pretrain = pretrain_size
 
-            def on_next(i):
+            def on_next(i):                
                 nonlocal pretrain
                 if pretrain > 0:
-                    if i.label is not None:
-                        model.learn_one(i.data, i.label)
-                        pretrain -= 1
+                    _learn_one(i)
+                    pretrain -= 1
                 else:
-                    predict = model.predict_one(i.data)
-                    if i.label is not None:
-                        model.learn_one(i.data, i.label)
-                    observer.on_next(Prediction(i, predict))
+                    predict = _predict(i)
+                    _learn_one(i)
+                    observer.on_next(predict)
 
             return source.subscribe(
                 on_next=on_next,
--- tests/evaluate/test_prequential.py
+++ tests/evaluate/test_prequential.py
@@ -1,37 +1,49 @@
 import rx
-import rxsci_multiflow as rsm
-from skmultiflow.anomaly_detection import HalfSpaceTrees
-from skmultiflow.data import AnomalySineGenerator, SEAGenerator
-from skmultiflow.trees import HoeffdingTreeClassifier
-
+import rxsci_river as rsr
+from river.anomaly import HalfSpaceTrees
+from river.tree import HoeffdingTreeClassifier
+from river import synth
 
 def test_prequential_hs_tree():
     actual_result = []
 
-    rsm.from_stream(AnomalySineGenerator(n_samples=2000, n_anomalies=10, random_state=1)).pipe(
-        rsm.evaluate.prequential(
-            model=HalfSpaceTrees(random_state=1),
-            pretrain_size=0),
+    source = [0.5, 0.45, 0.43, 0.44, 0.445, 0.45, 0.0]
+    rx.from_(source).pipe(
+        rsr.evaluate.prequential(
+            model=HalfSpaceTrees(
+                n_trees=5,
+                height=3,
+                window_size=3,
+                seed=42
+            ),
+            pretrain_size=3),
     ).subscribe(
         on_next=actual_result.append,
         on_error=print,
     )
     
     anomaly_count = 0
+    assert len(actual_result) == 4
     for p in actual_result:
-        if p.prediction[0] == 1:
+        if p > 0.8:
             anomaly_count += 1
-    assert len(actual_result) == 2000
-    #assert anomaly_count == 10  # issue: hstree detects 1748 anomalies in data
+    
+    assert anomaly_count == 1
 
 
 def test_prequential_hoeffding_tree():
     actual_result = []
 
-    rsm.from_stream(SEAGenerator(random_state=1), count=200).pipe(
-        rsm.evaluate.prequential(
-            model=HoeffdingTreeClassifier(),
-            pretrain_size=0),
+    gen = synth.Agrawal(classification_function=0, seed=42)
+
+    rx.from_(gen.take(1000)).pipe(
+        rsr.evaluate.prequential(
+            model=HoeffdingTreeClassifier(
+                grace_period=100,
+                split_confidence=1e-5,
+                nominal_attributes=['elevel', 'car', 'zipcode'],
+            ),
+            pretrain_size=100),
     ).subscribe(
         on_next=actual_result.append,
         on_error=print,
@@ -40,10 +52,10 @@ def test_prequential_hoeffding_tree():
     pred0_count = 0
     pred1_count = 0
     for p in actual_result:
-        if p.prediction[0] == 1:
+        if p == 1:
             pred1_count += 1
-        if p.prediction[0] == 0:
+        if p == 0:
             pred0_count += 1
-    assert len(actual_result) == 200
-    assert pred0_count == 55
-    assert pred1_count == 145
+    assert len(actual_result) == 900
+    assert pred0_count == 198
+    assert pred1_count == 702
